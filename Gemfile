67ba174c7da0b78b27d164f99e4b8e0de713630a
Prajwal Joshi <prazwal@outlook.com>
2016-04-23 21:02:48 +1000

    add bootstrap styling

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails', '4.2.6'
 gem 'pg', '~> 0.15'
 gem 'devise'
 gem 'milia'
+gem 'twitter-bootstrap-rails'
+gem 'devise-bootstrap-views'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,17 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_tenant!, :only => [ :index ]
 
   def index
+
+    if current_user
+      if sessions[:tenent_id]
+        Tenent.set_current_tenent session[:tenent_id]
+      else
+        Tenent.set_current_tenent current_user.tenents.first
+      end
+      @tenant = Tenant.current_tenant
+      params[:tenant_id] = @tenant.id
+    end
+
   end
+
 end
